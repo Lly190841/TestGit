82efaca61f82fd39917604ca13a4a1fd82befc6d
Aly190841 <somruedee.kha@kdc.co.th>
2023-01-07 14:14:00 +0700

    cotest commit

---
+++ test.py
@@ -0,0 +1,11 @@
+
+Question = input("Do you want Happy Brithday? (y/n) : ")
+X = "Y"
+Y = "y"
+
+if (Question==X or Question==Y):
+    print("Happ Brithday")
+else :
+    print("Okey then. Googbye!")
+
+    
